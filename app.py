5172480b7415d062b67e072d53f0ef6a6a8ef327
Firstname Lastname <a.gorkemunuvar@gmail.com>
2020-09-17 17:14:32 +0300

    initial commit

---
+++ app.py
@@ -0,0 +1,143 @@
+from flask import Flask, request
+from models import app, db, User, Note
+
+
+@app.route("/")
+def index():
+    return {"Welcome": "Home Page"}
+
+
+@app.route("/users", methods=["GET", "POST"])
+def user_api():
+    if request.method == "GET":
+        users = User.query.all()
+
+        result = [
+            {
+                "id": user.id,
+                "nick": user.nick,
+                "name": user.name,
+                "surname": user.surname,
+                "email": user.email,
+            }
+            for user in users
+        ]
+
+        return {"count": len(result), "users": result}
+
+    elif request.method == "POST":
+        if request.is_json:
+            json = request.get_json()
+
+            new_user = User(
+                json["nick"],
+                json["name"],
+                json["surname"],
+                json["email"],
+            )
+
+            db.session.add(new_user)
+            db.session.commit()
+
+            return {"message": f"User {new_user.nick} has been created successfully."}
+        else:
+            return {"error": "The request payload is not in JSON format"}
+
+
+@app.route("/user/<user_id>", methods=["GET", "PUT", "DELETE"])
+def handle_user(user_id):
+    if request.method == "GET":
+        user = User.query.filter_by(id=user_id).first()
+
+        if user is not None:
+            user_result = {
+                "id": user.id,
+                "nick": user.nick,
+                "name": user.name,
+                "surname": user.surname,
+                "email": user.email,
+            }
+
+            return {"user": user_result}
+        else:
+            return {"Error": "No user with that id!"}
+
+    elif request.method == "PUT":
+        if request.is_json:
+            user = User.query.filter_by(id=user_id).first()
+
+            if user is not None:
+                json = request.get_json()
+
+                user.nick = json["nick"]
+                user.name = json["name"]
+                user.surname = json["surname"]
+                user.email = json["email"]
+
+                db.session.add(user)
+                db.session.commit()
+
+                return {"Message": f"User {user.nick} has been updated."}
+
+            else:
+                return {"Error": "No user with that id!"}
+
+        else:
+            return {"Message": "The request payload is not in JSON format"}
+
+    elif request.method == "DELETE":
+        user = User.query.filter_by(id=user_id).first()
+
+        if user is not None:
+            db.session.delete(user)
+            db.session.commit()
+
+            return {"Message": f"User {user.nick} has been deleted."}
+
+        else:
+            return {"Error": "No user with that id!"}
+
+
+@app.route("/notes", methods=["GET", "POST"])
+def note_api():
+    if request.method == "GET":
+        notes = Note.query.all()
+
+        result = [
+            {
+                "id": note.id,
+                "note": note.note,
+                "time": note.time,
+                "location": note.location,
+                "street": note.street,
+                "city": note.city,
+                "user_id": note.user_id,
+            }
+            for note in notes
+        ]
+
+        return {"count": len(result), "notes": result}
+
+    elif request.method == "POST":
+        if request.is_json:
+            json = request.get_json()
+
+            new_note = Note(
+                json["note"],
+                json["time"],
+                json["location"],
+                json["street"],
+                json["city"],
+                json["user_id"],
+            )
+
+            db.session.add(new_note)
+            db.session.commit()
+
+            return {"message": f"User {new_note.note} has been created successfully."}
+        else:
+            return {"error": "The request payload is not in JSON format"}
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ models.py
@@ -0,0 +1,88 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+#########SETUP#########
+db_user = "geqhgdooimqsjo"
+db_password = "d7d1e02e9a60c7a8b3fb48ffcf944fb87e5876be35e9627073f780c3e7ac926c"
+db_name = "dagl5krkurfo4c"
+db_host = "ec2-52-31-233-101.eu-west-1.compute.amazonaws.com"
+db_port = "5432"
+
+app = Flask(__name__)
+
+app.config["SECRET_KEY"] = "secretkey"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config[
+    "SQLALCHEMY_DATABASE_URI"
+] = f"postgresql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+
+#########MODELS########
+class User(db.Model):
+    __tablename__ = "user"
+
+    id = db.Column(db.Integer, primary_key=True)
+    nick = db.Column(db.Text)
+    name = db.Column(db.Text)
+    surname = db.Column(db.Text)
+    email = db.Column(db.Text)
+    notes = db.relationship(
+        "Note", backref="user", cascade="all, delete, delete-orphan", lazy=True
+    )
+
+    def __init__(self, nick, name, surname, email):
+        self.nick = nick
+        self.name = name
+        self.surname = surname
+        self.email = email
+
+    def __repr__(self):
+        return (
+            f"<User id: %s nick: %s name: %s surname: %s email: %s>"
+            % self.id
+            % self.nick
+            % self.name
+            % self.surname
+            % self.email
+        )
+
+    def get_notes(self):
+        print(f"Notes by {user.name}")
+        for note in self.notes:
+            print(note.note)
+
+
+class Note(db.Model):
+    __tablename__ = "note"
+
+    id = db.Column(db.Integer, primary_key=True)
+    note = db.Column(db.Text)
+    time = db.Column(db.Text)
+    location = db.Column(db.Text)
+    street = db.Column(db.Text)
+    city = db.Column(db.Text)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+
+    def __init__(self, note, time, location, street, city, user_id):
+        self.note = note
+        self.time = time
+        self.location = location
+        self.street = street
+        self.city = city
+        self.user_id = user_id
+
+    def __repr__(self):
+        return (
+            f"<Note id: %s note: %s time: %s location: %s street: %s city: %s user_id: %s>"
+            % self.id
+            % self.note
+            % self.time
+            % self.location
+            % self.street
+            % self.city
+            % self.user_id
+        )
